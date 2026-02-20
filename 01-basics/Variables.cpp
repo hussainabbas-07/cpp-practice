bca89c7889cd0e029a0ad64b2708f87dbde80bfb
Hussain Abbas <syedhussain7241@gmail.com>
2026-02-20 21:34:07 +0500

    Added Variables.cpp to 01-basics

---
+++ 01-basics/Variables.cpp
@@ -0,0 +1,19 @@
+// Day2_Variables.cpp
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+	age = 18;
+
+	float gpa;
+	gpa = 3.8;
+
+	cout << "age:" << age << endl;
+	cout << "gpa:" << gpa << endl;
+
+}
+

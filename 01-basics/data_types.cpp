12d12bf08f3137a7a2450390c966134d19f8abc5
Hussain Abbas <syedhussain7241@gmail.com>
2026-02-20 21:51:05 +0500

    data_types.cpp added to 01-basics

---
+++ 01-basics/data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a = 10;
+    float b = 5.5;
+    char c = 'X';
+    double d = 3.14159;
+    bool flag = true;
+
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Char: " << c << endl;
+    cout << "Double: " << d << endl;
+    cout << "Bool: " << flag << endl;
+
+    return 0;
+}
